9f2448c29a4b0dc9bbbd193e0b5b4337e7482fd3
flebeau <frh.lebeau@gmail.com>
2017-12-15 21:02:00 +0100

    Few fixes

--- random_scenes.sh
+++ random_scenes.sh
@@ -2,6 +2,6 @@
 
 for i in $(seq 1 $1)
 do
-	build/raytracer -R -t 0.5 -m 0.25 -b 0.15 -n 40 --radius-max 10 > "scenes/random$i.scn"
+	build/raytracer -R -t 0.5 -m 0.35 -b 0.15 -n 40 --radius-max 10 > "scenes/random$i.scn"
 	build/raytracer -H 500 -W 500 -b 15 -r 10 --fov 70 -f "scenes/random$i.scn" -o "generated/random$i.bmp"
 done
